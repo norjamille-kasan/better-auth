3f065a740b298784247e2c2d3dffca85e843b378
Jam <jamclong0328@gmail.com>
2024-11-05 20:30:23 +0800

    Update get-config.ts

--- packages/cli/src/utils/get-config.ts
+++ packages/cli/src/utils/get-config.ts
@@ -24,7 +24,6 @@ possiblePaths = [
 	...possiblePaths.map((it) => `src/${it}`),
 	...possiblePaths.map((it) => `app/${it}`),
 	...possiblePaths.map((it) => `server/${it}`),
-	
 ];
 
 function stripJsonComments(jsonString: string): string {
